69537a5f4c541112ecb408aa47300c4da4c659a3
cesartalves <cesartorralvoa@hotmail.com>
2018-04-30 15:36:37 -0300

    bah

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -4,41 +4,37 @@ project="Unity-Sandbox"
 UNITY_EXECUTABLE="/Applications/Unity/Unity.app/Contents/MacOS/Unity"
 RESULTS_FILEPATH="$(pwd)/unitTestsResult.xml"
 
-echo "Attempting to build $project for Windows"
-time \
-  "$UNITY_EXECUTABLE" 
-  -batchmode 
-  -nographics 
-  -silent-crashes 
-  -logFile $(pwd)/unity.log 
-  -projectPath $(pwd) 
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" 
-  -quit
+# echo "Attempting to build $project for Windows"
+#   "$UNITY_EXECUTABLE" 
+#   -batchmode 
+#   -nographics 
+#   -silent-crashes 
+#   -logFile $(pwd)/unity.log 
+#   -projectPath $(pwd) 
+#   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" 
+#   -quit
 
-echo "Attempting to build $project for OS X"
-time \
-  "$UNITY_EXECUTABLE"  
-  -batchmode 
-  -nographics 
-  -silent-crashes 
-  -logFile $(pwd)/unity.log 
-  -projectPath $(pwd) 
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" 
-  -quit
+# echo "Attempting to build $project for OS X"
+#   "$UNITY_EXECUTABLE"  
+#   -batchmode 
+#   -nographics 
+#   -silent-crashes 
+#   -logFile $(pwd)/unity.log 
+#   -projectPath $(pwd) 
+#   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" 
+#   -quit
 
-echo "Attempting to build $project for Linux"
-time \
-  "$UNITY_EXECUTABLE"  
-  -batchmode 
-  -nographics 
-  -silent-crashes 
-  -logFile $(pwd)/unity.log 
-  -projectPath $(pwd) 
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" 
-  -quit
+# echo "Attempting to build $project for Linux"
+#   "$UNITY_EXECUTABLE"  
+#   -batchmode 
+#   -nographics 
+#   -silent-crashes 
+#   -logFile $(pwd)/unity.log 
+#   -projectPath $(pwd) 
+#   -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" 
+#   -quit
 
 echo "Running Editor Tests"
-time \
   "$UNITY_EXECUTABLE" \
   -projectPath $(pwd) \
   -batchmode \
@@ -52,7 +48,7 @@ echo ""
 echo "Results xml: $RESULTS_FILEPATH"
 
 
-echo 'Attempting to zip builds'
-zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
-zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
-zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
+# echo 'Attempting to zip builds'
+# zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
+# zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
+# zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
