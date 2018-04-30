bd0d936fb502f5dd6143ca1f921071773b4affde
cesartalves <cesartorralvoa@hotmail.com>
2018-04-30 13:52:47 -0300

    more changes...

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #! /bin/sh
 
-#BASE_URL=https://netstorage.unity3d.com/unity
-#HASH=88d00a7498cd
-#VERSION=5.5.1f1
-
 download() {
   file=$1
   url="https://download.unity3d.com/download_unity/fc1d3344e6ea/MacEditorInstaller/Unity-2017.3.1f1.pkg"
@@ -20,10 +16,5 @@ install() {
   sudo installer -dumplog -package `basename "$package"` -target /
 }
 
-# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
-# of available packages, where PLATFORM is `osx` or `win`
 
-# install "MacEditorInstaller/Unity-$VERSION.pkg"
-# install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
-# install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
-# install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
+
